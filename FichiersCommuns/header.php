82214215c39593f239b9d185bd2701be646af7fe
EstebanRacine <esteban.racineecole@gmail.com>
2023-05-07 15:42:41 +0200

    Header sans animation

--- FichiersCommuns/header.php
+++ FichiersCommuns/header.php
@@ -1,9 +1,13 @@
 <?php
+
+
+
 ?>
 
 <nav>
-    <a><img src="" alt=""></a>
-    <a href="index.php">Accueil</a>
+    <a href="index.php" id="navImg"><img src="images/Logo_DevelopER.png" alt="Mon Logo"></a>
+    <a href="index.php" class="underline">Accueil</a>
+    <a href="cv.php">Mon CV</a>
     <a href="projets.php">Mes projets</a>
     <a href="passions.php">Mes passions</a>
     <a href="contact.php">Contact</a>
