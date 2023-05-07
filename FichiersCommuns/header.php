750391ee30fcb931ba1cb070f99b7bb97b78f818
EstebanRacine <esteban.racineecole@gmail.com>
2023-05-07 14:34:40 +0200

    Start Header

--- FichiersCommuns/header.php
+++ FichiersCommuns/header.php
@@ -1,1 +1,10 @@
 <?php
+?>
+
+<nav>
+    <a><img src="" alt=""></a>
+    <a href="index.php">Accueil</a>
+    <a href="projets.php">Mes projets</a>
+    <a href="passions.php">Mes passions</a>
+    <a href="contact.php">Contact</a>
+</nav>
