6737386bb105335cdb45d8a763c19cf8326f600e
EstebanRacine <esteban.racineecole@gmail.com>
2023-05-07 17:59:50 +0200

    Page index mi responsive + images index

--- FichiersCommuns/footer.php
+++ FichiersCommuns/footer.php
@@ -3,7 +3,7 @@
 
 <footer>
     <div class="reseaux">
-        <a href=""></a>
-        <a href=""></a>
+        <a target="_blank" href="https://github.com/EstebanRacine"><i class="fa-brands fa-github"></i></a>
+        <a target="_blank" href="https://fr.linkedin.com/"><i class="fa-brands fa-linkedin-in"></i></a>
     </div>
 </footer>
--- index.php
+++ index.php
@@ -11,16 +11,51 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="CSS/index.css">
+    <link rel="shortcut icon" href="images/LogoOnglet.png" />
     <title>Accueil</title>
 </head>
 <body>
+
 <?php
 include "FichiersCommuns/header.php"
 ?>
 
+<div class="contenuIndex">
+    <div class="pres1">
+        <div class="textEcrit">
+            <h1 id="h1Start">Bienvenue</h1>
+        </div>
+        <div class="textEcrit">
+            <h1 id="h1Middle">sur le portfolio</h1>
+        </div>
+        <div class="textEcrit">
+            <h1 id="h1End">d'Esteban Racine</h1>
+        </div>
+        <i class="fa-solid fa-arrow-down" onclick="window.scrollBy(0, window.innerHeight);"></i>
 
+    </div>
+    <div class="quiSuisJe">
+        <h1>Qui Suis-je ?</h1>
+        <div class="infosPerso">
+            <img src="images/index/moi.jpg" alt="Image d'Esteban Racine">
+            <p>
+                Je suis Esteban RACINE, étudiant en BTS SIO au lycée Louis Pergaud. Je me suis toujours
+                interessé au fonctionnement des objets. À leurs conceptions, à leurs utilisations ou encore
+                à leurs utilisateurs. J'ai adoré comprendre le fonctionnement de la machine à vapeur ou
+                analyser le type de personnes qui vont acheter le dernier Iphone. Cet interêt pour la nouveauté
+                m'a amené vers le milieu de l'informatique. Je connaissais evidemment le coté exterieur et
+                visuel que nous cotoyons tous chaque jour mais depuis mon année de 1ère j'ai découvert
+                l'interieur, le développement plus particulièrement. Comprendre comment un site internet ou
+                une application mobile fonctionne me fascine, c'est pourquoi je me suis dirigé dans ce milieu
+                à la fois intéressant et important au vu du nombre d'utilisateur du net aujourd'hui.
+            </p>
+        </div>
+    </div>
+</div>
 
 
 <?php
