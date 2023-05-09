7f439d7abd9e55779ecbe418f168611f20a8e44e
Esteban Racine <esteban.racineecole@gmail.com>
2023-05-09 11:54:19 +0200

    Start Caroussel

---
+++ JS/slickProjets.js
@@ -0,0 +1,5 @@
+$('.carroussel').slick({
+    // slidesToShow :1,
+    // prevArrow  : "<i class=\"fa-solid fa-arrow-left\"></i>",
+    // nextArrow : "<i class=\"fa-solid fa-arrow-right\"></i>"
+});
--- projets.php
+++ projets.php
@@ -9,7 +9,9 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="CSS/projets.css">
     <title>Mes projets</title>
 </head>
 <body>
@@ -18,9 +20,35 @@
 include "FichiersCommuns/header.php";
 ?>
 
+<div class="contenuProjet">
+    <h1>Mes différents projets</h1>
+
+    <div class="carroussel">
+
+        <article id="ETC">
+            <h2>ETC (Enroule Ton Câble)</h2>
+            <a href="infoProj.php?projet=ETC">+ d'infos</a>
+        </article>
+
+        <article id="BestStudents">
+            <h2>BestStudents</h2>
+            <a href="infoProj.php?projet=BestStudents">+ d'infos</a>
+        </article>
+
+        <article id="Carottos">
+            <h2>Carottos</h2>
+            <a href="infoProj.php?projet=Carottos">+ d'infos</a>
+        </article>
+
+    </div>
+</div>
 
 <?php
 include "FichiersCommuns/footer.php";
 ?>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-migrate/3.4.1/jquery-migrate.min.js" integrity="sha512-KgffulL3mxrOsDicgQWA11O6q6oKeWcV00VxgfJw4TcM8XRQT8Df9EsrYxDf7tpVpfl3qcYD96BpyPvA4d1FDQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+<script src="JS/slickProjets.js"></script>
 </body>
 </html>
