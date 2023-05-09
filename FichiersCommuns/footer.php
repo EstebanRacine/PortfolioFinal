714d9a31c40f185e7426ee72a30393c93e22f808
Esteban Racine <esteban.racineecole@gmail.com>
2023-05-09 11:08:40 +0200

    Modif Footer

--- FichiersCommuns/footer.php
+++ FichiersCommuns/footer.php
@@ -6,4 +6,5 @@
         <a target="_blank" href="https://github.com/EstebanRacine"><i class="fa-brands fa-github"></i></a>
         <a target="_blank" href="https://fr.linkedin.com/"><i class="fa-brands fa-linkedin-in"></i></a>
     </div>
+    <p>2023</p>
 </footer>
