ec0e7088dc6e03247be093f9c499350deb6caab5
Esteban Racine <esteban.racineecole@gmail.com>
2023-05-16 11:48:42 +0200

    Projet responsive

--- JS/slickProjets.js
+++ JS/slickProjets.js
@@ -1,5 +1,6 @@
 $('.carroussel').slick({
-    // slidesToShow :1,
+    autoplay: true,
+    autoplaySpeed: 4000,
     prevArrow  : "<i class=\"fa-solid fa-arrow-left\"></i>",
     nextArrow : "<i class=\"fa-solid fa-arrow-right\"></i>"
 });
