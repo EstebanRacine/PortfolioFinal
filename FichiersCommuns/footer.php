c77ad9ff8dedcb91b44cf861147f4ea421fcb457
EstebanRacine <racine.esteban@lyceepergaud.fr>
2023-09-18 13:56:05 +0200

    Modif Index

--- FichiersCommuns/footer.php
+++ FichiersCommuns/footer.php
@@ -4,7 +4,7 @@
 <footer>
     <div class="reseaux">
         <a target="_blank" href="https://github.com/EstebanRacine"><i class="fa-brands fa-github"></i></a>
-        <a target="_blank" href="https://fr.linkedin.com/"><i class="fa-brands fa-linkedin-in"></i></a>
+        <a target="_blank" href="https://www.linkedin.com/in/esteban-racine-568844263/"><i class="fa-brands fa-linkedin-in"></i></a>
     </div>
     <p>2023</p>
 </footer>
