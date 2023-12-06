c59d5d693c4f0c4a39208396cba1356edcf56093
racinee <esteban.racineeecole@gmail.com>
2023-12-06 13:50:56 +0100

    Ajout Page Veille Technologique

---
+++ veille.php
@@ -0,0 +1,38 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="CSS/veille.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="images/LogoOnglet.png" />
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600&display=swap" rel="stylesheet">
+    <title>Veille Technologique</title>
+</head>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-DGNMBFFVZ2"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-DGNMBFFVZ2');
+</script>
+<body>
+<?php
+include "FichiersCommuns/header.php";
+?>
+<div class="contenuVeille">
+    <h1>Veille Technologique</h1>
+</div>
+<?php
+include "FichiersCommuns/footer.php";
+?>
+</body>
+</html>
